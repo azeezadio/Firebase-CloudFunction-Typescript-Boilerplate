44bc4e130d4b38494c3c5d58caede8271c7956a9
Adio Adeniran Azeez <adioadeniran@yahoo.co.uk>
2022-06-20 04:37:42 +0100

    write-to-firestore

--- src/index.ts
+++ src/index.ts
@@ -36,16 +36,4 @@ app.post('/store/transaction', async (req, res) => {
   }
 });
 
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin
-    .firestore()
-    .collection('messages')
-    .add({ original: original });
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
-
 exports.eriplatform = functions.https.onRequest(app);
